eda5bd9fab628f8dde5ca7cd0103086d1641443c
wangbo <wangbo@pset.suntec.net>
2015-04-20 15:40:30 +0800

    add controller

---
+++ FirstTest/WBTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  WBTestViewController.h
+//  FirstTest
+//
+//  Created by iOSDev on 15/4/20.
+//  Copyright (c) 2015年 iOSDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBTestViewController : UIViewController
+
+@end
---
+++ FirstTest/WBTestViewController.m
@@ -0,0 +1,37 @@
+//
+//  WBTestViewController.m
+//  FirstTest
+//
+//  Created by iOSDev on 15/4/20.
+//  Copyright (c) 2015年 iOSDev. All rights reserved.
+//
+
+#import "WBTestViewController.h"
+
+@interface WBTestViewController ()
+
+@end
+
+@implementation WBTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
